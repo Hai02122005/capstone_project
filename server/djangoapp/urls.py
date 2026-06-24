c77dd3f9ba9121fe116547bfc1cedd82d04a7a24
Đồng Hữu Hải <huuhaihd21@gmail.com>
2026-06-24 22:05:28 +0700

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,21 +1,22 @@
-# Uncomment the imports before you add the code
-# from django.urls import path
+from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-# from . import views
+from . import views
 
 app_name = 'djangoapp'
-urlpatterns = [
-    # login
-    path('login', views.login_user, name='login'),
 
-    # logout (nếu có)
+urlpatterns = [
+
+    path('login', views.login_user, name='login'),
     path('logout', views.logout_user, name='logout'),
 
-    # dealers
     path('get_dealers', views.get_dealerships, name='get_dealers'),
+    path('get_dealers/<str:state>', views.get_dealerships, name='get_dealers_by_state'),
 
-    # reviews
-    path('reviews', views.get_reviews, name='reviews'),
+    path('dealer/<int:dealer_id>', views.get_dealer_details, name='dealer_details'),
+
+    path('reviews/<int:dealer_id>', views.get_reviews, name='reviews'),
+
+    path('add_review', views.add_review, name='add_review'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
